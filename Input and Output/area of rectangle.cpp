4072279d7d2a4ac559a95a58a0860fa1f6be2395
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-25 23:11:43 +0530

    Create area of rectangle.cpp

---
+++ Input and Output/area of rectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"Enter length :- ";
+int l,b;
+cin>>l;
+cout<<"Enter width :- ";
+cin>>b;
+cout<<"Area of rectangle is = "<<l*b;
+return 0;
+}
