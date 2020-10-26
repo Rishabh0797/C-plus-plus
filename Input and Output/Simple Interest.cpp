a318c78c7c854a65b1c149ddc965748f5a90e8dc
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-26 18:54:30 +0530

    Create Simple Interest.cpp

---
+++ Input and Output/Simple Interest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float p,r,t;
+    cout<<"Enter Principle amount :- ";
+    cin>>p;
+    cout<<"Enter Interest rate :- ";
+    cin>>r;
+    cout<<"Enter Time period :- ";
+    cin>>t;
+    cout<<"Simple Interest is = "<<(p*r*t)/100;
+}
