351336ad0e4b25e17856f3f8ca8d716b79b33ac4
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-26 18:59:27 +0530

    Create area of circle.cpp

---
+++ Input and Output/area of circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   float r;
+   cout<<"Enter the circle radius :- ";
+   cin>>r;
+   cout<<"Area of circle is = "<<(r*r)*3.14;
+   return 0;
+}
