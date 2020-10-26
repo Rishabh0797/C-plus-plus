b6cb7d96b0bbc2594424ed484358202cd4e14034
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-26 19:04:23 +0530

    Create circumference of circle.cpp

---
+++ Input and Output/circumference of circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   float r;
+   cout<<"Enter the circle radius :- ";
+   cin>>r;
+   cout<<"Circumfrence of circle is = "<<(2*r)*3.14;
+   return 0;
+}
