e902bece4a22ca4fcd4a5c3b0f5081287a1e50ca
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-26 18:42:33 +0530

    Create perimeter of rectangle.cpp

---
+++ Input and Output/perimeter of rectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>;
+using namespace std;
+
+int main()
+{
+  int l,b;
+  cout<<"Enter length :- ";
+  cin>>l;
+  cout<<"Enter width :- ";
+  cin>>b;
+  cout<<"Perimeter of rectangle is = "<<2*(l+b);
+  return 0;
+}
