f7bdd6ead765f70c58eb4b24b5a0a51efc2952a5
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-28 18:00:35 +0530

    Create odd or even.cpp

---
+++ if and else/Simple Questions/odd or even.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter any number :- ";
+    int a;
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<a<<" is an Even Number.";
+    }
+    else
+    {
+        cout<<a<<" is an Odd Number.";
+    }
+
+    return 0;
+}
