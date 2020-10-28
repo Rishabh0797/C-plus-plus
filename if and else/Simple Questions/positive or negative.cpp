34dba7a3fa74253a16b4640c82ef985bb2ee402f
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-28 18:03:29 +0530

    Create positive or negative.cpp

---
+++ if and else/Simple Questions/positive or negative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter any number :- ";
+    int a;
+    cin>>a;
+    if(a<0)
+    {
+        cout<<a<<" is a Negative Number.";
+    }
+    else
+    {
+        cout<<a<<" is a Positive Number.";
+    }
+
+    return 0;
+}
