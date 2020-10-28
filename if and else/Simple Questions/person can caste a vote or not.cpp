fdd508e80141d2bde9a55a92c6d3b5c21a5902bb
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-10-28 18:10:01 +0530

    Create person can caste a vote or not.cpp

---
+++ if and else/Simple Questions/person can caste a vote or not.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter your Age :- ";
+    int a;
+    cin>>a;
+    if(a>17)
+    { 
+        cout<<"You are eligible to vote :) ";
+    }
+    else
+    {
+        cout<<"You are not eligible to vote :( ";
+    }
+
+    return 0;
+}
