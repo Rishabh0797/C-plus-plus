d7192da6c9908596a1a627168efcf26a96f46425
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-11-05 23:22:18 +0530

    Create print n natural number.cpp

---
+++ Loops/for loop/print n natural number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,num;
+    cout<<"Enter till where you want to print natural number : ";
+    cin>>num;
+    cout<<"\n";
+    for(i=0;i<=num;i++)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}
