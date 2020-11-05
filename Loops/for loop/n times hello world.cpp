b6a452a9aa52b7e6a8d6feffd682d382d6389df5
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-11-05 23:16:06 +0530

    Create n times hello world.cpp

---
+++ Loops/for loop/n times hello world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,num;
+    cout<<"Enter how many times you want to write 'Hello World' : ";
+    cin>>num;
+    cout<<"\n";
+    for(i=0;i<=num;i++)
+    {
+        cout<<"Hello World :) :)\n";
+    }
+    return 0;
+}
