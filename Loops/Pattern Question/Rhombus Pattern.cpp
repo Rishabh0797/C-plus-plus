35d575c6c63047dd1d1ef581180f98563fba7d26
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-11-17 14:15:51 +0530

    Create Rhombus Pattern.cpp

---
+++ Loops/Pattern Question/Rhombus Pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter value of n : ";
+    int i,j,n;
+    cin>>n;
+    cout<<"\n\n";
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=(n-i);j++)
+        {
+            cout<<"  ";
+        }
+        for(j=1;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
