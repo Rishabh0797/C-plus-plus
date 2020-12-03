461e4de95d785619d9f9ea9d495373b7442dae69
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-12-03 23:44:05 +0530

    Create Input and Output.cpp

---
+++ 2d-Array/Input and Output.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter size of column : ";
+    int r,c;
+    cin>>c;
+    cout<<"Enter size of row : ";
+    cin>>r;
+    int arr[r][c];
+    cout<<endl;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<"Enter "<<i<<j<<" element : ";
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
