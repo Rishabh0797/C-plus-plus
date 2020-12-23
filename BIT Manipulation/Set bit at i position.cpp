d5e05cd39504b8e5124fb1338214709fb5bb336d
Rishabh Bansal <58740285+Rishabh0797@users.noreply.github.com>
2020-12-23 17:11:03 +0530

    Create Set bit at i position.cpp

---
+++ BIT Manipulation/Set bit at i position.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    cout<<"Enter position: "<<endl;;
+    int i;
+    cin>>i;
+    int temp;
+    temp=n|(1<<i);
+    cout<<" After set bit of "<<n<<" at position "<<i<<" the answer is: "<<temp;;
+    return 0;
+}
